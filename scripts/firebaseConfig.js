3e232f0d99d39b9b56e6fae136299fe6faa83911
Andreas Huepgen <a.huepgen@web.de>
2026-01-22 13:15:57 +0100

    Delete scripts/firebaseConfig.js
    had to delete file. will pass it private

--- scripts/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBpgzyPWrtvfcmTmIr7wskgbj2-HiSxjqk",
-  authDomain: "join-2483.firebaseapp.com",
-  databaseURL: "https://join-2483-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "join-2483",
-  storageBucket: "join-2483.firebasestorage.app",
-  messagingSenderId: "1015631608333",
-  appId: "1:1015631608333:web:5dcfe60e31adb078aeb344"
-};
