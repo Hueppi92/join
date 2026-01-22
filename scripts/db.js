b2fd30039707bf032669b8914770e88782038b78
Hueppi92 <a.huepgen@web.de>
2026-01-22 13:08:44 +0100

    fixed firebase issues

--- scripts/db.js
+++ scripts/db.js
@@ -1,7 +1,4 @@
 // db.js
-firebase.initializeApp(firebaseConfig); // firebaseConfig kommt aus firebaseConfig.js
-
-// Realtime Database bereitstellen
+firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
-
 console.log("Firebase ready", db);
