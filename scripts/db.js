9900e8f584d6165f9c8b3ff6c3ed9bed177c3c44
Hueppi92 <a.huepgen@web.de>
2026-01-22 12:55:45 +0100

    changed dbConfig and removed api-key from git

--- scripts/db.js
+++ scripts/db.js
@@ -1,16 +1,5 @@
-// Firebase Konfiguration
-const firebaseConfig = {
-  apiKey: "AIzaSyCzNNFBCw9iLIMBmz4ItcS2ZslLeKfeStU",
-  authDomain: "join-2483.firebaseapp.com",
-  databaseURL: "https://join-2483-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "join-2483",
-  storageBucket: "join-2483.firebasestorage.app",
-  messagingSenderId: "1015631608333",
-  appId: "1:1015631608333:web:5dcfe60e31adb078aeb344"
-};
-
-// Firebase initialisieren
-firebase.initializeApp(firebaseConfig);
+// db.js
+firebase.initializeApp(firebaseConfig); // firebaseConfig kommt aus firebaseConfig.js
 
 // Realtime Database bereitstellen
 const db = firebase.database();
